703a47bf2d8ab47bb521631725c9ac93452806e8
kalerie <valeriiakuryliak@gmail.com>
2022-05-30 11:56:47 +0200

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,91 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { PreloadAllModules, RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventService,
+  EventResolver,
+  EventListResolver,
+  CreateSessionComponent,
+  SessionListComponent,
+  DurationPipe,
+  UpvoteComponent,
+  VoterService,
+  LocationValidator
+} from './events/index';
 
+import { 
+  JQ_TOKEN, 
+  TOASTR_TOKEN, 
+  Toastr, 
+  SimpleModalComponent,
+  CollapsibleWellComponent,
+  ModalTriggerDirective 
+} from './common/index';
+
+import { EventsAppComponent } from './events-app.component';
+import { NavBarComponent } from './nav/navbar.component';
+import { Error404Component } from './errors/404.component';
+
+import { appRoutes } from './routes';
+import { AuthService } from './user/auth.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+const toastr:Toastr = window['toastr'];
+const jQuery = window['$'];
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules}),
+    HttpClientModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavBarComponent,
+    EventDetailsComponent,
+    CreateEventComponent,
+    CreateSessionComponent,
+    Error404Component,
+    SessionListComponent,
+    CollapsibleWellComponent,
+    SimpleModalComponent,
+    ModalTriggerDirective,
+    DurationPipe,
+    UpvoteComponent,
+    LocationValidator
+  ],
+  providers: [
+    EventService,
+    {
+      provide: TOASTR_TOKEN, useValue: toastr
+    },
+    {
+      provide: JQ_TOKEN, useValue: jQuery
+    },
+    EventResolver,
+    { 
+      provide: 'canDeactivateCreateEvent', useValue: checkDirtyState 
+    },
+    EventListResolver,
+    AuthService,
+    VoterService
+  ],
+  bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
+
+export function checkDirtyState(component:CreateEventComponent) {
+  if(component.isDirty){
+    return window.confirm('You have not save this event, do you really want to cancel?');
+  }
+  return true;
+}
---
+++ src/app/nav/navbar.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { EventService, ISession } from '../events';
+import { AuthService } from '../user/auth.service';
+
+@Component({
+    selector: 'nav-bar',
+    templateUrl: './navbar.component.html',
+    styles: [`
+        .nav.nav-bar { font-size: 15px; }
+        #searchForm { margin-right: 100px; }
+        @media (max-width: 1200px) { #searchForm {display: none;}}
+        li > a.active { color: #F97924; }
+    `]
+})
+
+export class NavBarComponent {
+    searchTerm = "";
+    foundSessions: ISession[];
+
+    constructor(
+        public auth: AuthService,
+        private eventService: EventService
+        ) {
+        
+    }
+
+    searchSessions(searchTerm) {
+        this.eventService.searchSessions(searchTerm)
+            .subscribe(sessions => {
+                this.foundSessions = sessions;
+            })
+    }
+}
